66ece13d638edd7dbc83fd32c1c6f0a4a4a75f80
Guest User <Guest@Epicodus-5G.local>
2017-05-31 17:37:07 -0700

    add constants for the Trimet API call

---
+++ app/src/main/java/com/example/guest/wisecommute/Constants.java
@@ -0,0 +1,13 @@
+package com.example.guest.wisecommute;
+
+/**
+ * Created by Guest on 5/31/17.
+ */
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.Trimet_API_KEY;
+    public static final String API_BASE_URL = "https://developer.trimet.org/ws/v2/arrivals?locIDs=";
+    public static final String STOP_QUERY_ID = "locIDs"; //Example: "location"
+    public static final String API_FORMAT = "json=true";
+    public static final String API_KEY_QUERY_PARAMETER = "appID";
+}

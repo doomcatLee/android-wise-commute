7e981165a59ec319b2dd98c5a814740f1bfab898
Guest User <Guest@Epicodus-5G.local>
2017-05-31 18:14:22 -0700

    add arrival list

--- app/src/main/java/com/example/guest/wisecommute/StopListActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/StopListActivity.java
@@ -1,13 +1,109 @@
 package com.example.guest.wisecommute;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.guest.wisecommute.models.Train;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class StopListActivity extends AppCompatActivity {
+
+    public static final String TAG = StopListActivity.class.getSimpleName();
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private TrainListAdapter mAdapter;
+
+    public ArrayList<Train> mTrains = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_list);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String trainColor = intent.getStringExtra("trainColor");
+        String trainDirection = intent.getStringExtra("trainDirection");
+        String trainShortSign = intent.getStringExtra("trainShortSign");
+    }
+
+    public static final String TAG = RestaurantsListActivity.class.getSimpleName();
+    @Bind(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @Bind(R.id.locationTextView)
+    TextView mLocationTextView;
+    private RestaurantListAdapter mAdapter;
+
+    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
+
+        getRestaurants(location);
+    }
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mRestaurants = yelpService.processResults(response);
+
+                RestaurantsListActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), mRestaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(RestaurantsListActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+
+                        for(Restaurant restaurant : mRestaurants) {
+                            Log.d(TAG, "Name: " + restaurant.getName());
+                            Log.d(TAG, "Phone: " + restaurant.getPhone());
+                            Log.d(TAG, "Website: " + restaurant.getWebsite());
+                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
+                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
+                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
+                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
+                        }
+                    }
+                });
+
+                try {
+                    String jsonData = response.body().string();
+                    Log.d(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/guest/wisecommute/TrainFragmentDetail.java
@@ -0,0 +1,8 @@
+package com.example.guest.wisecommute;
+
+/**
+ * Created by Guest on 5/31/17.
+ */
+
+public class TrainFragmentDetail {
+}

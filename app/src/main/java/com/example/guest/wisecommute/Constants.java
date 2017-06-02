11f83e6a37f5344cbdddb51689c250dc15714c24
Riley Watts <rileywatts.yes@gmail.com>
2017-06-01 18:33:09 -0700

    change constants value to reflect proper response format

--- app/src/main/java/com/example/guest/wisecommute/Constants.java
+++ app/src/main/java/com/example/guest/wisecommute/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     public static final String API_KEY = BuildConfig.Trimet_API_KEY;
     public static final String API_BASE_URL = "https://developer.trimet.org/ws/v2/arrivals?locIDs=";
     public static final String STOP_QUERY_ID = "locIDs"; //Example: "location"
-    public static final String API_FORMAT = "json=true";
+    public static final String API_FORMAT = "json";
     public static final String API_KEY_QUERY_PARAMETER = "appID";
 }

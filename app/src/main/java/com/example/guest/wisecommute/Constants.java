14143d172091ef8ffc24a4bb9737acc0e3aa91fa
znergy <ryanjonesirl@gmail.com>
2017-06-06 16:53:52 -0700

    delete locID= from the base url

--- app/src/main/java/com/example/guest/wisecommute/Constants.java
+++ app/src/main/java/com/example/guest/wisecommute/Constants.java
@@ -8,7 +8,7 @@ import com.example.guest.wisecommute.models.Stop;
 
 public class Constants {
     public static final String API_KEY = BuildConfig.Trimet_API_KEY;
-    public static final String API_BASE_URL = "https://developer.trimet.org/ws/v2/arrivals?locIDs=";
+    public static final String API_BASE_URL = "https://developer.trimet.org/ws/v2/arrivals?";
     public static final String STOP_QUERY_ID = "locIDs"; //Example: "location"
     public static final String API_FORMAT = "json";
     public static final String API_KEY_QUERY_PARAMETER = "appID";

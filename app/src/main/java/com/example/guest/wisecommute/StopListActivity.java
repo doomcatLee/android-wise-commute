1df7064f27dfcb6ff952816b3ebb45f3a2ac7f07
znergy <ryanjonesirl@gmail.com>
2017-06-06 14:28:53 -0700

    add logic to dynamically switch what array list of stops will show

--- app/src/main/java/com/example/guest/wisecommute/StopListActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/StopListActivity.java
@@ -3,11 +3,16 @@ package com.example.guest.wisecommute;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.example.guest.wisecommute.adapters.StopAdapter;
+import com.example.guest.wisecommute.models.Stop;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,9 +22,7 @@ public class StopListActivity extends AppCompatActivity {
     public static final String TAG = StopListActivity.class.getSimpleName();
     @Bind(R.id.stopListView) ListView lvStopList;
     @Bind(R.id.tvTrainDirectionFullSign) TextView tvTrainDirectionFullSign;
-
-    // hashmap (northbound/southbound -> stopList array)
-    String[] stopNames = {"Gateway", "Lents/Foster", "Holgate"};
+    ArrayList<Stop> stopList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +36,34 @@ public class StopListActivity extends AppCompatActivity {
         final String trainDirectionFullSign = intent.getStringExtra("trainDirectionFullSign");
         final String trainShortSign = intent.getStringExtra("trainShortSign");
 
+        if (trainColor.equals("green") && trainDirection.equals("clackamas")) {
+            setStopList(Constants.greenToClackamasStops);
+        } else if (trainColor.equals("green") && trainDirection.equals("city center")) {
+            setStopList(Constants.greenToCityCenterStops);
+        } else if (trainColor.equals("blue") && trainDirection.equals("hillsboro")) {
+            setStopList(Constants.blueToHillsboroStops);
+        } else if (trainColor.equals("blue") && trainDirection.equals("gresham")) {
+            setStopList(Constants.blueToGreshamStops);
+        } else if (trainColor.equals("red") && trainDirection.equals("airport")) {
+            setStopList(Constants.redToAirportStops);
+        } else if (trainColor.equals("red") && trainDirection.equals("beaverton")) {
+            setStopList(Constants.redToBeavertonStops);
+        } else if (trainColor.equals("orange") && trainDirection.equals("milwaukie")) {
+            setStopList(Constants.orangeToMilwaukieStops);
+        } else if (trainColor.equals("orange") && trainDirection.equals("city center")) {
+            setStopList(Constants.orangeToCityCenterStops);
+        } else if (trainColor.equals("yellow") && trainDirection.equals("expo")) {
+            setStopList(Constants.yellowToExpoStops);
+        } else if (trainColor.equals("yellow") && trainDirection.equals("city center")) {
+            setStopList(Constants.yellowToCityCenterStops);
+        }
+
+        Log.d(TAG, "onCreate: stopList is " + stopList.toString());
+
         tvTrainDirectionFullSign.setText(trainDirectionFullSign);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stopNames);
-        lvStopList.setAdapter(adapter);
+        StopAdapter stopAdapter = new StopAdapter(this, stopList);
+        lvStopList.setAdapter(stopAdapter);
 
         lvStopList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,5 +77,11 @@ public class StopListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void setStopList(Stop[] stops) {
+        for(int i = 0; i < stops.length; i++) {
+            stopList.add(stops[i]);
+        }
     }
 }

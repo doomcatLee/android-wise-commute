01a4cbcd840dad3766345944943aeb5a8706a413
znergy <ryanjonesirl@gmail.com>
2017-06-06 14:29:41 -0700

    delete unused code

--- app/src/main/java/com/example/guest/wisecommute/TrainColorActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/TrainColorActivity.java
@@ -1,14 +1,11 @@
 package com.example.guest.wisecommute;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.GridView;
-import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,7 +27,6 @@ public class TrainColorActivity extends AppCompatActivity implements View.OnClic
     @Bind(R.id.yellowToExpo) Button yellowToExpo;
     @Bind(R.id.yellowToCityCenter) Button yellowToCityCenter;
     @Bind(R.id.gridLayout) GridLayout gridLayout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

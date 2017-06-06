885d7e75ab076de906a5772e1f122519ef9e6b42
znergy <ryanjonesirl@gmail.com>
2017-06-06 14:26:27 -0700

    add 5 constants, which hold the stop name and stop id for green, blue, red, yellow, and orange line trains. The array of stop objects will then be applied to the listview based off the user's choice of train color, and train direction\!

---
+++ app/src/main/java/com/example/guest/wisecommute/adapters/StopAdapter.java
@@ -0,0 +1,8 @@
+package com.example.guest.wisecommute.adapters;
+
+/**
+ * Created by administrator on 6/6/17.
+ */
+
+public class StopAdapter {
+}
---
+++ app/src/main/java/com/example/guest/wisecommute/models/Stop.java
@@ -0,0 +1,8 @@
+package com.example.guest.wisecommute.models;
+
+/**
+ * Created by administrator on 6/6/17.
+ */
+
+public class Stop {
+}

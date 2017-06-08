9d5fbdfc4c65f9869e11906a45d8836cfdf6fb08
znergy <ryanjonesirl@gmail.com>
2017-06-08 16:45:27 -0700

    add an update to the user model allowing for email, name, uid

--- app/src/main/java/com/example/guest/wisecommute/models/User.java
+++ app/src/main/java/com/example/guest/wisecommute/models/User.java
@@ -1,8 +1,37 @@
 package com.example.guest.wisecommute.models;
 
+import android.net.Uri;
+
 /**
  * Created by administrator on 6/7/17.
  */
 
 public class User {
+    private String email;
+    private String name;
+    private String uid;
+    private Uri profileImage;
+
+    public User(String email, String name, String uid, Uri profileImage) {
+        this.email = email;
+        this.name = name;
+        this.uid = uid;
+        this.profileImage = profileImage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public Uri getProfileImage() {
+        return profileImage;
+    }
 }

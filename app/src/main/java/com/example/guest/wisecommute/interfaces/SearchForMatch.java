6d9ab89f776f84f29dfbe383f11a8dea82006aa8
znergy <ryanjonesirl@gmail.com>
2017-06-08 16:44:53 -0700

    add minor changes

--- app/src/main/java/com/example/guest/wisecommute/interfaces/SearchForMatch.java
+++ app/src/main/java/com/example/guest/wisecommute/interfaces/SearchForMatch.java
@@ -1,8 +1,13 @@
 package com.example.guest.wisecommute.interfaces;
 
+import com.example.guest.wisecommute.models.User;
+
+import java.util.ArrayList;
+
 /**
  * Created by administrator on 6/7/17.
  */
 
 public interface SearchForMatch {
+    void searchForMatch(ArrayList<User> users);
 }

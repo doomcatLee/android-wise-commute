b1c9a53b226710677b77cc88b1e4e384918e0a8c
znergy <ryanjonesirl@gmail.com>
2017-06-08 16:39:30 -0700

    add DashboardActivity which will eventually show a twitter feed for trimet updates and alerts

--- app/src/main/java/com/example/guest/wisecommute/DashboardActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/DashboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.guest.wisecommute;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    /** Menu Code */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        } else if (id == R.id.action_settings) {
+            Intent intent = new Intent(DashboardActivity.this, SettingsActivity.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.action_dashboard) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Logout Firebase User */
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }

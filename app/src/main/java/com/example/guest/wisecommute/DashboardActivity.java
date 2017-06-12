8d03e1cdbd75652feed5b2c0bd3dc3ddf50ac5ff
znergy <ryanjonesirl@gmail.com>
2017-06-12 08:33:41 -0700

    add minor changes

--- app/src/main/java/com/example/guest/wisecommute/DashboardActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/DashboardActivity.java
@@ -4,15 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        btnMainActivity = (Button) findViewById(R.id.btnMainActivity);
+
+        btnMainActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnMainActivity) {
+            Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     /** Menu Code */

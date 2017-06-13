d8cbca50e3502db5f88aeab703b23e35918945ec
znergy <ryanjonesirl@gmail.com>
2017-06-12 17:11:47 -0700

    add minor changes

--- app/src/main/java/com/example/guest/wisecommute/TrainArrivalActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/TrainArrivalActivity.java
@@ -2,6 +2,7 @@ package com.example.guest.wisecommute;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.guest.wisecommute.adapters.TrainListAdapter;
@@ -25,11 +28,18 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class TrainArrivalActivity extends AppCompatActivity {
+public class TrainArrivalActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = TrainArrivalActivity.class.getSimpleName();
-    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    @Bind(R.id.twitterRecyclerView) RecyclerView mRecyclerView;
     @Bind(R.id.tvStopName) TextView tvStopName;
+    @Bind(R.id.ivAlarm) ImageView ivAlarm;
     private TrainListAdapter mAdapter;
+
+    private String trainColor;
+    private String trainStopID;
+    private String trainDirection;
+    private String stopName;
+    private String trainShortSign;
 
     public ArrayList<Train> mTrains = new ArrayList<>();
 
@@ -41,27 +51,35 @@ public class TrainArrivalActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starts");
 
         Intent intent = getIntent();
-        String trainColor = intent.getStringExtra("trainColor");
-        String trainDirection = intent.getStringExtra("trainDirection");
-        String trainStopID = intent.getStringExtra("stopID");
-        String stopName = intent.getStringExtra("stopName");
-        String trainDirectionFullSign = intent.getStringExtra("trainDirectionFullSign");
-        String trainShortSign = intent.getStringExtra("trainShortSign");
-
-        // set up the adapter..
+        trainColor = intent.getStringExtra("trainColor");
+        trainDirection = intent.getStringExtra("trainDirection");
+        trainStopID = intent.getStringExtra("stopID");
+        stopName = intent.getStringExtra("stopName");
+        trainShortSign = intent.getStringExtra("trainShortSign");
 
         tvStopName.setText(stopName);
+
+        ivAlarm.setOnClickListener(this);
 
         getTrains(trainColor, trainStopID, trainDirection, trainShortSign);
 
         Log.d(TAG, "onCreate: ends");
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == ivAlarm) {
+            Intent openClockIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
+            openClockIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(openClockIntent);
+        }
+    }
+
     /** Menu Code */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
+        inflater.inflate(R.menu.menu_arrival, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -80,6 +98,10 @@ public class TrainArrivalActivity extends AppCompatActivity {
             Intent intent = new Intent(TrainArrivalActivity.this, DashboardActivity.class);
             startActivity(intent);
             finish();
+        } else if (id == R.id.action_refresh) {
+            Log.d(TAG, "onOptionsItemSelected: clicked");
+            // trigger api call
+            getTrains(trainColor, trainStopID, trainDirection, trainShortSign);
         }
         return super.onOptionsItemSelected(item);
     }
@@ -118,37 +140,15 @@ public class TrainArrivalActivity extends AppCompatActivity {
                         mRecyclerView.setLayoutManager(layoutManager);
                         mRecyclerView.setHasFixedSize(true);
 
-                        for(Train train : mTrains) {
-                            Log.d(TAG, "ShortSign: " + train.getShortSign());
-                            Log.d(TAG, "FullSign: " + train.getFullSign());
-//                            Long estimated = Long.parseLong(train.getEstimated());
-//                            Date startDate = new Date();
-//                            Date date = new Date(estimated);
-//                            long difference = date.getTime() - startDate.getTime();
-//                            Date differenceDate = new Date(difference);
-//                            DateFormat format = new SimpleDateFormat("mm");
-//                            format.setTimeZone(TimeZone.getTimeZone("UTC-8:00"));
-//                            String estimatedMinutes = format.format(differenceDate);
-//                            Log.d(TAG, "Estimated: " + estimatedMinutes + " minutes");
-                            Log.d(TAG, train.getEstimatedMinutes());
-
-//                            /** Scheduled Time (hours) */
-//                            Long scheduled = Long.parseLong(train.getScheduled());
-//                            Date scheduledDate = new Date(scheduled);
-//                            DateFormat scheduledHoursFormat = new SimpleDateFormat("hh");
-//                            scheduledHoursFormat.setTimeZone(TimeZone.getTimeZone("PST"));
-//                            String scheduledHours = scheduledHoursFormat.format(scheduledDate);
-//                            Integer convertedHours = Integer.parseInt(scheduledHours) + 5;
+//                        for(Train train : mTrains) {
+//                            Log.d(TAG, "ShortSign: " + train.getShortSign());
+//                            Log.d(TAG, "FullSign: " + train.getFullSign());
+//                            Log.d(TAG, train.getEstimatedMinutes());
 //
-//                            /** Scheduled Time (minutes) */
-//                            DateFormat scheduledMinutesFormat = new SimpleDateFormat("mm");
-//                            scheduledMinutesFormat.setTimeZone(TimeZone.getTimeZone("PST"));
-//                            String scheduledMinutes = scheduledMinutesFormat.format(scheduledDate);
-
-                            Log.d(TAG, train.getScheduledTime());
-
-                            Log.d(TAG, "StopID: " + train.getLocID());
-                        }
+//                            Log.d(TAG, train.getScheduledTime());
+//
+//                            Log.d(TAG, "StopID: " + train.getLocID());
+//                        }
                     }
                 });
             }

0a73d1a2d67e9a2a841b385df570046b541e8b8f
znergy <ryanjonesirl@gmail.com>
2017-06-12 17:09:39 -0700

    add a livefeedfragment which will handle showing the twitter api call, which will give trimet updates about trains running:

--- app/src/main/java/com/example/guest/wisecommute/LiveFeedFragment.java
+++ app/src/main/java/com/example/guest/wisecommute/LiveFeedFragment.java
@@ -1,18 +1,42 @@
 package com.example.guest.wisecommute;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.guest.wisecommute.adapters.TwitterAdapter;
+import com.example.guest.wisecommute.models.Tweet;
+import com.example.guest.wisecommute.services.TwitterService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LiveFeedFragment extends Fragment {
+    private static final String TAG = LiveFeedFragment.class.getSimpleName();
+    @Bind(R.id.twitterRecyclerView) RecyclerView mRecyclerView;
+    @Bind(R.id.tvTitle) TextView tvStopName;
+    private TwitterAdapter mAdapter;
+    private Context mContext;
 
+    public ArrayList<Tweet> mTweets = new ArrayList<>();
 
     public LiveFeedFragment() {
         // Required empty public constructor
@@ -22,8 +46,51 @@ public class LiveFeedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ButterKnife.bind(getActivity());
+
+        getTweets("trimet", "2");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_live_feed, container, false);
+    }
+
+    private void getTweets(String screenName, String count) {
+        Log.d(TAG, "getTweets: starts");
+        final TwitterService twitterService = new TwitterService();
+        twitterService.findTweets(screenName, count, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "onFailure: API Call failed with " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                Log.d(TAG, "onResponse: starts");
+                mTweets = twitterService.processResults(response);
+                Log.d(TAG, "onResponse: mTweets is " + mTweets);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        mAdapter = new TwitterAdapter(getActivity(), mTweets);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+
+                        for(Tweet tweet : mTweets) {
+                            Log.d(TAG, "Twitter - TimeStamp: " + tweet.getTimeStamp());
+                            Log.d(TAG, "Twitter - ScreenName: " + tweet.getScreenName());
+                            Log.d(TAG, "Twitter - Location: " + tweet.getLocation());
+                            Log.d(TAG, "Twitter - Text: " + tweet.getText());
+                            Log.d(TAG, "Twitter - Name: " + tweet.getUsername());
+                        }
+                    }
+                });
+            }
+        });
+        Log.d(TAG, "getTrains: ends");
     }
 
 }

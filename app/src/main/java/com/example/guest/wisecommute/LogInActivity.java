5bcb0fcb9fee87770a68bc835456bd939aaccf4f
znergy <ryanjonesirl@gmail.com>
2017-06-12 17:10:04 -0700

    add minor changes

--- app/src/main/java/com/example/guest/wisecommute/LogInActivity.java
+++ app/src/main/java/com/example/guest/wisecommute/LogInActivity.java
@@ -62,7 +62,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null) {
-                    Intent intent = new Intent(LogInActivity.this, TrainColorActivity.class);
+                    Intent intent = new Intent(LogInActivity.this, MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();

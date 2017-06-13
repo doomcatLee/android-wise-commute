bf063020fd3a290d75b1cb5c8f89d80178941468
znergy <ryanjonesirl@gmail.com>
2017-06-12 17:12:16 -0700

    add a twitter adapter for handling setting values to the recyclerview for the twitter api

--- app/src/main/java/com/example/guest/wisecommute/adapters/TwitterAdapter.java
+++ app/src/main/java/com/example/guest/wisecommute/adapters/TwitterAdapter.java
@@ -1,8 +1,70 @@
 package com.example.guest.wisecommute.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.guest.wisecommute.R;
+import com.example.guest.wisecommute.models.Tweet;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by administrator on 6/12/17.
  */
 
-public class TwitterAdapter {
+public class TwitterAdapter extends RecyclerView.Adapter<TwitterAdapter.TwitterViewHolder> {
+    private ArrayList<Tweet> mTweets = new ArrayList<>();
+    private Context mContext;
+
+    public TwitterAdapter(Context context, ArrayList<Tweet> tweets) {
+        mContext = context;
+        mTweets = tweets;
+    }
+
+    // View Holder which holds the train list item (train arrival information format)
+    @Override
+    public TwitterAdapter.TwitterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.twitter_list_item, parent, false);
+        TwitterViewHolder viewHolder = new TwitterViewHolder(view);
+        return viewHolder;
+    }
+
+    // updates the contents of the ItemView to reflect the restaurant in the given position
+    @Override
+    public void onBindViewHolder(TwitterAdapter.TwitterViewHolder holder, int position) {
+        holder.bindTweet(mTweets.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    // ViewHolder will find the views and set their values for the item in the list
+    public class TwitterViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.tvText) TextView tvText;
+        @Bind(R.id.tvTimeStamp) TextView tvTimeStamp;
+        @Bind(R.id.tvScreenName) TextView tvScreenName;
+        @Bind(R.id.tvName) TextView tvName;
+
+        public TwitterViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindTweet(Tweet tweet) {
+            tvText.setText("Text: " + tweet.getText());
+            tvScreenName.setText("Screen Name: " + tweet.getScreenName());
+            tvName.setText("Name: " + tweet.getUsername());
+            tvTimeStamp.setText("Time: " + tweet.getTimeStamp());
+        }
+    }
 }

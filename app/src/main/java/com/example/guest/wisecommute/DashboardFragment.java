0957f35dfd96773cce1a595741694a57268c3b93
znergy <ryanjonesirl@gmail.com>
2017-06-14 08:23:07 -0700

    add the spinners for user preferences

--- app/src/main/java/com/example/guest/wisecommute/DashboardFragment.java
+++ app/src/main/java/com/example/guest/wisecommute/DashboardFragment.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DashboardFragment extends Fragment {
+    @Bind(R.id.sColor) Spinner sColor;
+    @Bind(R.id.sDirection) Spinner sDirection;
+    @Bind(R.id.sHome) Spinner sHome;
+    @Bind(R.id.sWork) Spinner sWork;
 
 
     public DashboardFragment() {
@@ -22,6 +30,8 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ButterKnife.bind(getActivity());
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
